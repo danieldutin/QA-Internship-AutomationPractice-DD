d93e4378248f3a33e6a01d6b818c211ac6b5f104
Daniel Dutin <danieldutin@gmail.com>
2021-04-27 15:35:02 +0300

    All login, and Create account tests - created

--- src/main/java/page/NewUserPage.java
+++ src/main/java/page/NewUserPage.java
@@ -6,29 +6,28 @@ import org.openqa.selenium.WebDriver;
 public class NewUserPage {
 
     private WebDriver driver;
-    // private By emailField = By.id("email");
-    // private By createButton = By.id("SubmitCreate");
     private By accountForm = By.xpath("//*[@id=\"columns\"]/div[1]/span[2]");
     private By mr_Title = By.id("uniform-id_gender1");
     private By mrs_Title = By.id("uniform-id_gender2");
     private By firstName = By.id("customer_firstname");
     private By lastName = By.id("customer_lastname");
-    private By password = By.id("passwd");
+    private By passWord = By.id("passwd");
     private By dayDropDown = By.id("days");
     private By exactDay = By.xpath("//*[@id=\"days\"]/option[2]");
     private By monthDropDown = By.id("months");
     private By exactMonth = By.xpath("//*[@id=\"months\"]/option[2]");
     private By yearDropDown = By.id("years");
-    private By exactYear = By.xpath("//*[@id=\"years\"]/option[2000]");
-    private By address = By.id("address1");
-    private By city = By.id("city");
+    private By exactYear = By.xpath("//*[@id=\"years\"]/option[21]");
+    private By addressLineOne = By.id("address1");
+    private By cityInput = By.id("city");
     private By stateDropDown = By.id("id_state");
     private By exactState = By.xpath("//*[@id=\"id_state\"]/option[4]");
     private By zipCode = By.id("postcode");
-    private By countryDropDown = By.id("//*[@id=\"id_state\"]/option[21]");
-    private By exactCountry = By.xpath("//*[@id=\"id_state\"]/option[4]");
+    //private By countryDropDown = By.id("//*[@id=\"id_state\"]/option[21]");
+    //private By exactCountry = By.xpath("//*[@id=\"id_state\"]/option[4]");
     private By mobileP = By.id("phone_mobile");
     private By regButton = By.id("submitAccount");
+
 
     public NewUserPage(WebDriver driver) {
         this.driver = driver;
@@ -39,5 +38,84 @@ public class NewUserPage {
         return driver.findElement(accountForm).getText();
     }
 
+    public void setTitle(String pass) {
+
+        if (pass.equals("Mr.")) {
+            driver.findElement(mr_Title).click();
+        } else if (pass.equals("Mrs.")) {
+            driver.findElement(mrs_Title).click();
+        }
+    }
+
+    public void setFirstName(String firstname) {
+
+        driver.findElement(firstName).sendKeys(firstname);
+    }
+
+    public void setLastName(String lastname) {
+
+        driver.findElement(lastName).sendKeys(lastname);
+    }
+
+    public void setPassword(String password) {
+
+        driver.findElement(passWord).sendKeys(password);
+    }
+
+    public void selectDayDropDown() {
+
+        driver.findElement(dayDropDown).click();
+        driver.findElement(exactDay).click();
+    }
+
+    public void selectMonthDropDown() {
+
+        driver.findElement(monthDropDown).click();
+        driver.findElement(exactMonth).click();
+    }
+
+    public void selectYearDropDown() {
+
+        driver.findElement(yearDropDown).click();
+        driver.findElement(exactYear).click();
+    }
+
+    public void setAddressLineOne(String address) {
+
+        driver.findElement(addressLineOne).sendKeys(address);
+    }
+
+    public void setCity(String city) {
+
+        driver.findElement(cityInput).sendKeys(city);
+    }
+
+    public void setZipCode(String zip) {
+
+        driver.findElement(zipCode).sendKeys(zip);
+    }
+
+    public void selectStateDropDown() {
+
+        driver.findElement(stateDropDown).click();
+        driver.findElement(exactState).click();
+    }
+
+    /*public void selectCountryDropDown() {
+
+        driver.findElement(countryDropDown).click();
+        driver.findElement(exactCountry).click();
+    }*/
+
+    public void setMobileP(String mobile) {
+
+        driver.findElement(mobileP).sendKeys(mobile);
+    }
+
+    public AccountPage clickRegButton() {
+
+        driver.findElement(regButton).click();
+        return new AccountPage(driver);
+    }
 
 }
--- src/test/java/CreateAcc/CreateAccTests.java
+++ src/test/java/CreateAcc/CreateAccTests.java
@@ -1,7 +1,11 @@
 package CreateAcc;
 
 import base.BaseTests;
+import com.beust.jcommander.IStringConverter;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
+import page.AccountPage;
 import page.CreateAccountPage;
 import page.HomePage;
 import page.NewUserPage;
@@ -11,13 +15,13 @@ import static org.testng.Assert.assertTrue;
 
 public class CreateAccTests extends BaseTests {
 
+    ConfigFileReader reader = new ConfigFileReader();
 
     @Test
     public void validEmailTest() throws Exception {
 
         HomePage homePage = new HomePage(startWebDriver());
 
-        ConfigFileReader reader = new ConfigFileReader();
         String email = reader.getProperty("newEmail");
         String authentication = reader.getProperty("authentication");
 
@@ -30,9 +34,46 @@ public class CreateAccTests extends BaseTests {
 
     }
 
-    /*@Test
-    public void accountCreatedTest(){
+    @Test
+    public void accountCreatedTest() throws Exception{
 
+        HomePage homePage = new HomePage(startWebDriver());
 
-    }*/
+        String email = reader.getProperty("anotherEmail");
+        //String authentication = reader.getProperty("authentication");
+        String title = reader.getProperty("title");
+        String firstName = reader.getProperty("firstName");
+        String lastName = reader.getProperty("lastName");
+        String passWord = reader.getProperty("passWord");
+        String addressLineOne = reader.getProperty("addressLineOne");
+        String cityInput = reader.getProperty("cityInput");
+        String zipCode = reader.getProperty("zipCode");
+        String mobileP = reader.getProperty("mobileP");
+        String welcome = reader.getProperty("welcome");
+
+        CreateAccountPage createAccountPage = homePage.clickSignUpButtonCreate();
+        createAccountPage.setEmailField(email);
+        NewUserPage newUserPage = createAccountPage.clickCreateButton();
+
+        newUserPage.setTitle(title);
+        newUserPage.setFirstName(firstName);
+        newUserPage.setLastName(lastName);
+        newUserPage.setPassword(passWord);
+        newUserPage.selectDayDropDown();
+        newUserPage.selectMonthDropDown();
+        newUserPage.selectYearDropDown();
+        newUserPage.setAddressLineOne(addressLineOne);
+        newUserPage.setCity(cityInput);
+        newUserPage.selectStateDropDown();
+        newUserPage.setZipCode(zipCode);
+        newUserPage.setMobileP(mobileP);
+
+        AccountPage accountPage = newUserPage.clickRegButton();
+
+        String myAccountText = accountPage.getAlertText();
+
+        assertTrue(myAccountText.contains(welcome), "Incorrect data");
+
+    }
+
 }
