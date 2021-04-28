75e2fefaa342037c1974ea3b99f8f50f19146b09
Daniel Dutin <danieldutin@gmail.com>
2021-04-28 15:18:44 +0300

    Checkout Product with Guest User - Complete

--- src/test/java/Checkout/RegisteredUserCheckoutTests.java
+++ src/test/java/Checkout/RegisteredUserCheckoutTests.java
@@ -1,5 +1,54 @@
 package Checkout;
 
-public class RegisteredUserCheckoutTests {
+import Payment.CheckoutPage;
+import ProductDisplayPage.ProductDetailPage;
+import ProductListingPage.WomanPage;
+import base.BaseTests;
+import helper.ConfigFileReader;
+import org.testng.annotations.Test;
+import page.AccountPage;
+import page.HomePage;
+import page.LoginPage;
 
+import static org.testng.Assert.assertTrue;
+
+public class RegisteredUserCheckoutTests extends BaseTests {
+    ConfigFileReader reader = new ConfigFileReader();
+
+    @Test
+    public void addProductToCart() throws Exception {
+
+        HomePage homePage = new HomePage(startWebDriver());
+
+        ConfigFileReader reader = new ConfigFileReader();
+        String email = reader.getProperty("loginEmail");
+        String pass = reader.getProperty("pass");
+
+        LoginPage loginPage = homePage.clickSignUpButtonLogIn();
+        loginPage.setEmailField(email);
+        loginPage.setPasswordField(pass);
+        AccountPage accountPage = loginPage.clickSignUpButton();
+
+        String expectedResult = reader.getProperty("orderComplete");
+
+        WomanPage womanPage = homePage.clickWomanButton();
+
+        ProductDetailPage productDetailPage = womanPage.clickOnProduct();
+
+        CheckoutPage checkoutPage = productDetailPage.clickAddToCartBtn();
+        checkoutPage.waitForLoad(driver);
+        Thread.sleep(2000);
+        checkoutPage.proceedToCheckout();
+        checkoutPage.summaryProceed();
+        checkoutPage.addressProceed();
+        checkoutPage.clickOnCheckBox();
+        checkoutPage.shippingProceed();
+        checkoutPage.clickPayByWire();
+        checkoutPage.clickConfirm();
+
+        String actualResult = checkoutPage.getAlertText();
+
+        assertTrue(actualResult.contains(expectedResult), "Invalid operation");
+
+    }
 }
